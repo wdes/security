c9b83784d7f7ea9a3bb362674cb16e109490edd8
William Desportes <williamdes@wdes.fr>
2023-06-23 18:58:45 +0200

    Add IPv6 addresses for CloudFront

--- make-aws-cloudfront-range.sh
+++ make-aws-cloudfront-range.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 curl -f -s -# https://ip-ranges.amazonaws.com/ip-ranges.json | jq -r '.prefixes[] | select(.service == "CLOUDFRONT") | .ip_prefix' | sort > cloudfront-ips.txt
+curl -f -s -# https://ip-ranges.amazonaws.com/ip-ranges.json | jq -r '.ipv6_prefixes[] | select(.service == "CLOUDFRONT") | .ipv6_prefix' | sort >> cloudfront-ips.txt
 #curl https://d7uri8nf7uskq.cloudfront.net/tools/list-cloudfront-ips | jq -r '.CLOUDFRONT_GLOBAL_IP_LIST  | join("\n")' | sort > cloudfront-ips.txt
