91060878a6ca30008281db453b097f8e15c9fa28
William Desportes <williamdes@wdes.fr>
2023-07-26 14:01:49 +0200

    Sort AWS CloudFront IPs

--- make-aws-cloudfront-range.sh
+++ make-aws-cloudfront-range.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # See: https://stackoverflow.com/a/69768584/5155484
-curl -f -s -# https://ip-ranges.amazonaws.com/ip-ranges.json | jq -r '.prefixes[] | select(.service == "CLOUDFRONT") | .ip_prefix' | sort > cloudfront-ips.txt
-curl -f -s -# https://ip-ranges.amazonaws.com/ip-ranges.json | jq -r '.ipv6_prefixes[] | select(.service == "CLOUDFRONT") | .ipv6_prefix' | sort >> cloudfront-ips.txt
+curl -f -s -# https://ip-ranges.amazonaws.com/ip-ranges.json | jq -r '.prefixes[] | select(.service == "CLOUDFRONT") | .ip_prefix' | sort -V > cloudfront-ips.txt
+curl -f -s -# https://ip-ranges.amazonaws.com/ip-ranges.json | jq -r '.ipv6_prefixes[] | select(.service == "CLOUDFRONT") | .ipv6_prefix' | sort -V >> cloudfront-ips.txt
 
 # Does not seem up to date: 06-2023
 #curl https://d7uri8nf7uskq.cloudfront.net/tools/list-cloudfront-ips | jq -r '.CLOUDFRONT_GLOBAL_IP_LIST  | join("\n")' | sort > cloudfront-ips.txt
