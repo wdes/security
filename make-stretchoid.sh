8462d844653a9009129db534fe2efeecf2ff7cff
William Desportes <williamdes@wdes.fr>
2023-07-26 14:52:54 +0200

    [data] Change the IP format for stretchoid
    Ref: https://github.com/SilvrrGIT/IP-Lists/issues/85#issuecomment-1651708481
    Ref: https://github.com/SilvrrGIT/IP-Lists/issues/85

--- make-stretchoid.sh
+++ make-stretchoid.sh
@@ -14,7 +14,7 @@ mv stretchoid_revisions/$REV.sorted.txt stretchoid_revisions/$REV.txt
 awk -F'#' '{print $2" # "$1}' OFS=, "stretchoid_revisions/$REV.txt" | awk '{$1=$1;print}' | sort > stretchoid_revisions/$REV-reversed.txt
 
 # Sort by name and reverse the list to build the list of all possible IPs
-cat stretchoid_revisions/v*-reversed.txt | LC_ALL=C.UTF-8 sort -t "-" -n | uniq | awk -F'#' '{print $2" # "$1}' OFS='#' | awk '{$1=$1;print}' > ../stretchoid.txt
+cat stretchoid_revisions/v*-reversed.txt | LC_ALL=C.UTF-8 sort -t "-" -n | uniq | awk -F'#' '{print "# "$1" \n "$2}' OFS='#' | awk '{$1=$1;print}' > ../stretchoid.txt
 
 # Search for false positives
 # cat ../stretchoid.txt | cut -d '#' -f 1 | xargs -P 50 -I {} bash -c 'set -eu;rev="$(dig @9.9.9.9 +short +time=1 +tries=1 -x {})"; if [[ "$rev" == *";;"* ]]; then sleep 1; rev="$(dig @8.8.8.8 +short +time=1 +tries=1 -x {})"; fi; echo "{} # $rev";' | grep -v -F "stretchoid.com"
