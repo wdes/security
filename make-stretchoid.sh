f28a983a4b9080451246dcc33a7e8faf0307feb1
William Desportes <williamdes@wdes.fr>
2023-07-30 10:01:06 +0200

    Add chunk counts

--- make-stretchoid.sh
+++ make-stretchoid.sh
@@ -16,5 +16,7 @@ awk -F'#' '{print $2" # "$1}' OFS=, "stretchoid_revisions/$REV.txt" | awk '{$1=$
 # Sort by name and reverse the list to build the list of all possible IPs
 cat stretchoid_revisions/v*-reversed.txt | LC_ALL=C.UTF-8 sort -t "-" -n | uniq | awk -F'#' '{print "# "$1" \n "$2}' OFS='#' | awk '{$1=$1;print}' > ../stretchoid.txt
 
+grep -F '#' ../stretchoid.txt | cut -d- -f2 | grep -P '^[0-9]{3,}+' | sort | uniq -c | sort' > ./stretchoid-chunk-counts.txt
+
 # Search for false positives
 # cat ../stretchoid.txt | cut -d '#' -f 1 | xargs -P 50 -I {} bash -c 'set -eu;rev="$(dig @9.9.9.9 +short +time=1 +tries=1 -x {})"; if [[ "$rev" == *";;"* ]]; then sleep 1; rev="$(dig @8.8.8.8 +short +time=1 +tries=1 -x {})"; fi; echo "{} # $rev";' | grep -v -F "stretchoid.com"
