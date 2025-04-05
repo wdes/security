7f270a8a508917b7bfdf5578b997fcf7f995b963
William Desportes <williamdes@wdes.fr>
2025-04-05 14:39:01 +0200

    More documentation

--- scripts/make-aws-cloudfront-range.sh
+++ scripts/make-aws-cloudfront-range.sh
@@ -2,6 +2,7 @@
 
 PROJECT_ROOT="$(realpath $(dirname $0)/../)"
 
+# Last checked: 05/04/2025
 echo "Running in: $PROJECT_ROOT"
 
 set -eu
@@ -10,5 +11,5 @@ set -eu
 curl -f -s -# https://ip-ranges.amazonaws.com/ip-ranges.json | jq -r '.prefixes[] | select(.service == "CLOUDFRONT") | .ip_prefix' | sort -V > "$PROJECT_ROOT/data/collections/amazon/cloudfront-ips.txt"
 curl -f -s -# https://ip-ranges.amazonaws.com/ip-ranges.json | jq -r '.ipv6_prefixes[] | select(.service == "CLOUDFRONT") | .ipv6_prefix' | sort -V >> "$PROJECT_ROOT/data/collections/amazon/cloudfront-ips.txt"
 
-# Does not seem up to date: 06-2023
-#curl https://d7uri8nf7uskq.cloudfront.net/tools/list-cloudfront-ips | jq -r '.CLOUDFRONT_GLOBAL_IP_LIST  | join("\n")' | sort > "$PROJECT_ROOT/data/collections/amazon/cloudfront-ips.txt"
+# Does not seem up to date: 06-2023, 04-2025 (missing some ranges & all IPv6)
+#curl https://d7uri8nf7uskq.cloudfront.net/tools/list-cloudfront-ips | jq -r '.CLOUDFRONT_GLOBAL_IP_LIST  | join("\n")' | sort -V > "$PROJECT_ROOT/data/collections/amazon/cloudfront-ips.txt"
